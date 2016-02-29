5bde4ea05e79709db9dda226234d32bdfdb7cf19
itssuyog96 <itssuyog96@gmail.com>
2016-03-01 00:19:27 +0530

    Sync correct

--- project-edit-opt.php
+++ project-edit-opt.php
@@ -1,21 +1,13 @@
 <?php
 	function editdel()
 	{
-<<<<<<< HEAD
+
 		echo '<div class="space"></div><div class="row project-nav">'.
 		'<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12"><a href="#" class="btn btn-default block">Edit</a>'.
 		'</div>'.
 		'<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12"><a href="#" class="btn btn-default block">Delete</i>'.
 			 '</a>'.
 			 '</div>'.
-=======
-		echo
-		'<div class="row project-nav">'.
-			'<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12"><a href="#" class="btn btn-default btn-prev block"><i class="fa fa-angle-left"></i>Edit</a>'.
-			'</div>'.
-			'<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12"><a href="#" class="btn btn-default btn-next block">Delete<i class="fa fa-angle-right"></i></a>'.
-			'</div>'.
->>>>>>> origin/master
 		'</div>';
 	}		
 ?>
