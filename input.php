ea63d7d96be9bf57389c87288d73463b41f588a7
Omkar Dubas <chandandubas@gmail.com>
2016-02-29 16:55:58 +0530

    align: center the buttons

--- input.php
+++ input.php
@@ -182,7 +182,7 @@
 				<div class="box-white padding-all">
 				<form action="123.php">
 					<div class="row">
-						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 ">
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 btn btn-align:center">
 							<input type="hidden" name="type" value="project" >
 							<input type="submit" class="btn btn-dark" value="CREATE NEW PROJECT">
 							
